b9a561b41248cb1e8ce04238835c8db4b0cd31a7
GByteos <gbyte@gb-tronix.de>
2023-05-08 22:01:09 +0200

    Added new project for the main app

---
+++ Yet-on-track/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Yet_on_track;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Yet-on-track/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Yet_on_track;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
