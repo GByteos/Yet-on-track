a44b85041dd2688e817817d24e2218a7f53f1652
GByteos <gbyte@gb-tronix.de>
2023-05-09 21:25:56 +0200

    Added first base models

---
+++ Yet-on-track/Model/Activity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yet_on_track.Model
+{
+    internal class Activity
+    {
+    }
+}
