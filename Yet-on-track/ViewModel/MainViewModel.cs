1bc9171fd0810e00cf110c828906e7512e5ace7f
GByteos <gbyte@gb-tronix.de>
2023-05-09 21:01:03 +0200

    Basic setup stuff
    Added MVVM and MAUI Community toolkit
    Wiered up the main vie and view model to the di

---
+++ Yet-on-track/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yet_on_track.ViewModel;
+
+public partial class MainViewModel : ObservableObject
+{
+    [RelayCommand]
+    public void PushNotification()
+    {
+        Shell.Current.DisplayAlert("Button Clicked", "The button was clicked", "OK");
+    }
+}
